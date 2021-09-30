61f13c6a08de2d0ba0075c5175c2b094ad611d8e
Neo <neochronos.chaos9@gmail.com>
2021-09-30 14:08:20 +0200

    Fixed 4-puts.c and compiled it into a.out

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 int main()
 {
-  char str[80] = "Programming is like building a multilingual puzzle";
-  puts(str \n);
+  char str[80] = "\"Programming is like building a multilingual puzzle";
+  puts(str);
   
     return 0;
 }
