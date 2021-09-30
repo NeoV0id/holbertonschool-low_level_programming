21de4a2ebd7d31fab55e6b28ab10f9bc16d2f36d
Neo <neochronos.chaos9@gmail.com>
2021-09-30 16:32:18 +0200

    Fixed 4-puts according to betty-doc

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+/**
+ * main-This function will print the following string using puts.
+ * Return: return 0
+ */
 int main(void)
 {
-/*This function will print the following string using puts.*/
 char str[80] = "\"Programming is like building a multilingual puzzle";
 puts(str);
 return (0);
