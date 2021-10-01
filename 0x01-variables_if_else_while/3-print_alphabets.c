ff4b89e2292ede99adb684252bd4641904109f53
Neo <neochronos.chaos9@gmail.com>
2021-10-01 17:08:51 +0200

    Fixed 0-positive_or_negative and added two c files

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,11 +11,17 @@ int main(void)
 {
 
 char a[26] = "abcdefghijklmnopqrstuvwxyz";
+char b[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int i;
 for
 (i = 0; i < 26; i++)
 {
 putchar(a[i]);
+}
+for
+(i = 0; i < 26; i++)
+{
+putchar(b[i]);
 }
 putchar('\n');
 
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,26 @@
 
 int main(void)
 {
-
 char a[26] = "abcdefghijklmnopqrstuvwxyz";
 int i;
-for
-(i = 0; i < 26; i++)
+i = 0;
+ while
+/**
+ * We'll use for instead of while, describe what i does and then we'll put a while
+ */
+  (i < 26)
+{
+i++;
+if
+(i != 5)
 {
 putchar(a[i]);
+}
+else if
+(i != 17)
+{
+putchar(a[i]);
+}
 }
 putchar('\n');
 
