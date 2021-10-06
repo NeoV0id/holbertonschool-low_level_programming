299a3b0661739a188a8d7a9c6454476a5411b7a1
Neo <neochronos.chaos9@gmail.com>
2021-10-06 20:55:19 +0200

    Fixed 6 according to betty-style

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -9,13 +9,13 @@
 
 int _abs(int n)
 {
-    if (n > 0)
-    {
-        return (n);
-    }
-    else if (n < 0)
-    {
-        return (n = n * -1);
-    }
-    return (0);
+if (n > 0)
+{
+return (n);
+}
+else if (n < 0)
+{
+return (n = n * -1);
+}
+return (0);
 }
