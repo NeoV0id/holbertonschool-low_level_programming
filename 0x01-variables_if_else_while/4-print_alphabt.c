6fe7c2f133b17b92bca0c3d67cda51000dbb25d2
Neo <neochronos.chaos9@gmail.com>
2021-10-06 12:32:21 +0200

    Fixed 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,8 +2,7 @@
 
 /**
  * main - this function will print out the alphabet
- * putchar - will display the alphabet (replace printf)
- * @char: is the string of alphabet
+ *
  * Return: will return 0
  */
 
@@ -11,25 +10,18 @@ int main(void)
 {
 char a[26] = "abcdefghijklmnopqrstuvwxyz";
 int i;
-i = 0;
- while
-/**
- * We'll use for instead of while, describe what i does and then we'll put a while
- */
-  (i < 26)
+
+for
+	(i = 0;
+	 i < 26;
+	 i++)
 {
-i++;
-if
-(i != 5)
-{
-putchar(a[i]);
+	if (!(i == 4 || i == 16))
+	{
+	putchar(a[i]);
+	}
 }
-else if
-(i != 17)
-{
-putchar(a[i]);
-}
-}
+
 putchar('\n');
 
 return (0);
