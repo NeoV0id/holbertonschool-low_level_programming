8895be4487f438b7ef69e91cbf0365df72e2ad73
Neo <neochronos.chaos9@gmail.com>
2021-10-06 15:30:35 +0200

    Fixed 0-_putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,22 @@
-#include <unistd.h>
+#include "main.h"
+
 /**
  * main - will print out a string with putchar
- * @char: contains my string
+ *
  * Return: will return 0 (success)
  */
+
 int main(void)
 {
-	char c[9] = "_putchar";
-	int i;
-
-	for
-		(i = 0;
-		 i < 9;
-		 i++);
-	write(1, &c, 8);
-	write(1, "\n", 1);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
