acdef92900f2673ea2b170b3249d0aca71f8ddc5
Neo <neochronos.chaos9@gmail.com>
2021-10-06 20:52:11 +0200

    Added code to files from 6 to 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - will add two integer and prints out result
+ *
+ * Return: Always 0 (success)
+ */
+
+int add(int a, int b)
+{
+	int c = (a+b);
+
+	return (c);
+}
