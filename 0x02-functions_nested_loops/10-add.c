9afd1e8fad090c79e7e174ad28ab9fbe364f476d
Neo <neochronos.chaos9@gmail.com>
2021-10-06 20:58:49 +0200

    Fixed 10-add.c according to betty-style and betty-doc

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,15 @@
 
 /**
  * add - will add two integer and prints out result
+ * @a - first int to add
+ * @b - second int to add
  *
  * Return: Always 0 (success)
  */
 
 int add(int a, int b)
 {
-	int c = (a+b);
+	int c = (a + b);
 
 	return (c);
 }
