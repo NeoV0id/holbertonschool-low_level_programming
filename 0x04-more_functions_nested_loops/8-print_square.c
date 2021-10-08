5dce8a2aa2234d328199cca58c07e9091b5c9cdc
Neo <neochronos.chaos9@gmail.com>
2021-10-08 13:44:19 +0200

    Modified files for project 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -13,9 +13,9 @@ void print_square(int size)
 
 	c = 35;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i <= size; i++)
 	{
-		for (j = 0; j < size; j++)
+		for (j = 0; j <= size; j++)
 		{
 			_putchar(c);
 		}
