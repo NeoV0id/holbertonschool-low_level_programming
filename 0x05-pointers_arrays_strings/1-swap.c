c27940ebf4bce9bb582ebd9c48f9ee8f99192db1
Neo <neochronos.chaos9@gmail.com>
2021-10-11 13:07:21 +0200

    Added 1-swap and its prototype to main.h

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the value of to integer
+ * @a: integer a (to switch with int b)
+ * @b: integer b
+ */
+
+void swap_int(int *a, int *b)
+{
+	int *c = 0;
+
+	*c = *a;
+	*a = *b;
+	*b = *c;
+}
