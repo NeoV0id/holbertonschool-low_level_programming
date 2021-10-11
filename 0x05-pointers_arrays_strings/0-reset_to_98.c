b496d8f90fd6a2ca7e835b61b31126894aca7947
Neo <neochronos.chaos9@gmail.com>
2021-10-11 12:37:51 +0200

    Fixed 0 according to betty-doc

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - takes a pointer to an int and assign the value of 98 to it
- * @*n - is the pointer to change
+ * @n - is the pointer to change
  */
 
 void reset_to_98(int *n)
