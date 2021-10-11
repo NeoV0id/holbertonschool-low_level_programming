e8bfedc126e968ded01d08a712203b50134fc2e0
Neo <neochronos.chaos9@gmail.com>
2021-10-11 12:27:44 +0200

    Added main.h as well as _putchar.c and its prototype to the header and 0-reset_to_98.c as well as its prototype to the header

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int and assign the value of 98 to it
+ * @*n - is the pointer to change
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
