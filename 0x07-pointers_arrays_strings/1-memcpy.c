4d270e9a6b3ca5bf5288e34bd24f738f10faf8c0
Neo <neochronos.chaos9@gmail.com>
2021-10-18 13:19:00 +0200

    Fixed 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,14 +12,10 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	unsigned int j;
 
 	for (i = 0; i < n; i++)
 	{
-		for (j = 0; j < n; j++)
-		{
-			src[i] = dest[j];
-		}
+		dest[i] = src[i];
 	}
 
 	return (dest);
