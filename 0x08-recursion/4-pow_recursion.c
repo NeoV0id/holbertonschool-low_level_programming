5aa51c017d9923445ffe05014cedb540615f46d5
Neo <neochronos.chaos9@gmail.com>
2021-10-20 16:59:14 +0200

    Complete ex04

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: number to raise
+ * @y: power
+ *
+ * Return: value of x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int n;
+
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	else if (x < 0)
+	{
+		return (-1);
+	}
+
+	n = _pow_recursion(x, y/2);
+
+	if (y % 2 == 0)
+	{
+		return (n * n);
+	}
+
+	return (x * n * n);
+}
