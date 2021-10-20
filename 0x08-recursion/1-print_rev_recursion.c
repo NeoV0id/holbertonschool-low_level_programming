cc74eecb4f1e0a6b069b4ef6f678e4ec1fb901b4
Neo <neochronos.chaos9@gmail.com>
2021-10-20 13:37:02 +0200

    Complete ex01

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - function that prints a reversed string, followed by a
+ * new line
+ * @s: string to print
+ */
+
+void _print_rev_recursion(char *s)
+{
+
+
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
