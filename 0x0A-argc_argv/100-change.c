f7106e4fea50eeb971acfcf6daba2561d3bf2cb6
Neo <neochronos.chaos9@gmail.com>
2021-10-21 21:37:16 +0200

    Fixed 100 according to betty

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * main - prints the minimum number of coins to change for an amount of money.
  * @argc: integer
@@ -8,7 +7,6 @@
  *
  * Return: 0 for success, 1 for failure
  */
-
 int main(int argc, char *argv[])
 {
 	int c;
@@ -22,43 +20,32 @@ int main(int argc, char *argv[])
 
 		return (1);
 	}
-
 	c = atoi(argv[1]);
-
 	while (c > 0)
 	{
 		d++;
-
 		if ((c - 25) >= 0)
 		{
 			c -= 25;
-
 			continue;
 		}
-
 		if ((c - 10) >= 0)
 		{
 			c -= 10;
 			continue;
 		}
-
 		if ((c - 5) >= 0)
 		{
 			c -= 5;
-
 			continue;
 		}
 		if ((c - 2) >= 0)
 		{
 			c -= 2;
-
 			continue;
 		}
-	c--;
-
+		c--;
 	}
-
 	printf("%d\n", d);
-
 	return (0);
 }
