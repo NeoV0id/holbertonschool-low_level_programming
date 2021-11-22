add878553c1cf0ee41c0d2a8220e97361344bb2e
Neo <neochronos.chaos9@gmail.com>
2021-11-22 13:51:02 +0100

    Fixed files

---
+++ 0x00-hello_world/4-puts.s
@@ -0,0 +1,66 @@
+	.file	"4-puts.c"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$96, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movabsq	$7881706611452039202, %rax
+	movabsq	$2338328219430578541, %rdx
+	movq	%rax, -96(%rbp)
+	movq	%rdx, -88(%rbp)
+	movabsq	$7599087837537593708, %rax
+	movabsq	$2333181710560748652, %rdx
+	movq	%rax, -80(%rbp)
+	movq	%rdx, -72(%rbp)
+	movabsq	$7956009416907322733, %rax
+	movabsq	$8824082329461945703, %rdx
+	movq	%rax, -64(%rbp)
+	movq	%rdx, -56(%rbp)
+	movq	$6646906, -48(%rbp)
+	movq	$0, -40(%rbp)
+	movq	$0, -32(%rbp)
+	movq	$0, -24(%rbp)
+	leaq	-96(%rbp), %rax
+	movq	%rax, %rdi
+	call	puts@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L3
+	call	__stack_chk_fail@PLT
+.L3:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
---
+++ 0x00-hello_world/my_lib.h
@@ -0,0 +1,6 @@
+#ifndef MY_LIB_H
+#define MY_LIB_H
+
+int my_putchar(char c);
+
+#endif
