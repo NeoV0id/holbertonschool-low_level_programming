6bb7a32089a6af323c41fd26757e2b35e195ece3
Neo <neochronos.chaos9@gmail.com>
2022-01-07 23:26:38 +0100

    Tried to fix 7 and added 9

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -27,7 +27,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	for (i = 0; temp != NULL; i++)
+	for (i = 0; temp != NULL && i != idx; i++)
 	{
 		if (i == idx)
 		{
