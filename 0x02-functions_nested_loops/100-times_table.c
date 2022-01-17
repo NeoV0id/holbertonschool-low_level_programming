d232548aa9d187cffedaa6c756e9117497a583b7
Neo <neochronos.chaos9@gmail.com>
2022-01-17 14:04:12 +0100

    Fixed errors

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,7 +27,7 @@ void print_times_table(int n)
 					printf(",   ");
 				else if ((j * i) < 100 && (j * i) >= 10)
 					printf(",  ");
-				else if ((j * i) >= 100)
+				else if ((j * i) > 100)
 					printf(", ");
 			}
 		}
