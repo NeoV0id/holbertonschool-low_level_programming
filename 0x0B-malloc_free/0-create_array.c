14f411a8d677e9d02af3fda26b4cdb4c8c22cb96
Neo <neochronos.chaos9@gmail.com>
2022-02-12 17:05:41 +0100

    Fixed 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,21 +18,11 @@ char *create_array(unsigned int size, char c)
 	arr = malloc(sizeof(char)*size);
 
 	if (arr == NULL)
-	{
 		return (NULL);
-	}
 	else if (size == 0)
-	{
-		return (0);
-	}
+		return (NULL);
 
-	else
-	{
-		for (i = 0; i <= size; i++)
-		{
-			arr[i] = c;
-		}
-	}
+	arr[0] = c;
 
 	return (arr);
 }
