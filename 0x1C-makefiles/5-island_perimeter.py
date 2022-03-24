4ab18eca3484dd649ad22b1aa59b2908b763833f
NeoV0id <neo@LAPTOP-M3VPT85L.localdomain>
2022-03-24 10:56:57 +0100

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" 
+Module for task 5
+"""
+
+
+def island_perimeter(grid):
+    """ island_perimeter: will compute the perimeter of the island on grid 
+        args:
+            grid: grid to check
+    """
+    re_water = lambda grid, water : grid.remove(water)
+    perimeter = 0
+    island = []
+    for i in grid:
+        for j in i: 
+            if j == 0:
+                pass
+            elif j == 1:
+                island.append(j)
+    print(island)
+    for i in range(0, len(island)):
+        if i != 0:
+            if i + 1 == len(island):
+                perimeter += 3
+                print("c")
+            elif i + 1 != len(island):
+                perimeter += 2
+                print("b")
+        elif i == 0:
+            if i != len(island):
+                perimeter += 3
+                print("a.a")
+            elif i == len(island):
+                perimeter += 4
+                print("a.b")
+
+    return perimeter
+
