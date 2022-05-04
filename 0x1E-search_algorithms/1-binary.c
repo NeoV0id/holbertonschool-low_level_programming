f60c70c2af5ab202ce6f14fbb1cabf9d3a31dddb
NeoV0id <neov0id.nova@gmail.com>
2022-05-04 14:55:31 -0700

    Fix betty

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,7 +13,7 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-        unsigned int i, j, n;
+	unsigned int i, j, n;
 
 	if (array == NULL)
 		return (-1);
@@ -47,15 +47,15 @@ int binary_search(int *array, size_t size, int value)
  */
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
 }
